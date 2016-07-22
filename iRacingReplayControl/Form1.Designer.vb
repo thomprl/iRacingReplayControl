493aab733aee3a5a07429421460219f5a9d69429
Ricky Thompson <rickyt29@gmail.com>
2016-07-22 10:31:11 -0500

    V1.0

---
+++ iRacingReplayControl/Form1.Designer.vb
@@ -0,0 +1,75 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.startButton = New System.Windows.Forms.Button()
+        Me.playButton = New System.Windows.Forms.Button()
+        Me.statusLabel = New System.Windows.Forms.Label()
+        Me.SuspendLayout()
+        '
+        'startButton
+        '
+        Me.startButton.Location = New System.Drawing.Point(160, 56)
+        Me.startButton.Name = "startButton"
+        Me.startButton.Size = New System.Drawing.Size(75, 23)
+        Me.startButton.TabIndex = 0
+        Me.startButton.Text = "Start"
+        Me.startButton.UseVisualStyleBackColor = True
+        '
+        'playButton
+        '
+        Me.playButton.Location = New System.Drawing.Point(160, 108)
+        Me.playButton.Name = "playButton"
+        Me.playButton.Size = New System.Drawing.Size(75, 23)
+        Me.playButton.TabIndex = 1
+        Me.playButton.Text = "Play"
+        Me.playButton.UseVisualStyleBackColor = True
+        '
+        'statusLabel
+        '
+        Me.statusLabel.AutoSize = True
+        Me.statusLabel.Location = New System.Drawing.Point(13, 178)
+        Me.statusLabel.Name = "statusLabel"
+        Me.statusLabel.Size = New System.Drawing.Size(39, 13)
+        Me.statusLabel.TabIndex = 2
+        Me.statusLabel.Text = "Label1"
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(292, 269)
+        Me.Controls.Add(Me.statusLabel)
+        Me.Controls.Add(Me.playButton)
+        Me.Controls.Add(Me.startButton)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents startButton As Button
+    Friend WithEvents playButton As Button
+    Friend WithEvents statusLabel As Label
+End Class
